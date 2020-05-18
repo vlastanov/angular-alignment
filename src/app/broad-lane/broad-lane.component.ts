d9ef99b09ca6e9efed50994759b1c154d0eb0f84
vlastanov <vlastanov@hotmail.com>
2020-05-18 14:28:56 +0300

    1 file modified

--- src/app/broad-lane/broad-lane.component.ts
+++ src/app/broad-lane/broad-lane.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  NgModel,
+  FormBuilder,
+  FormGroup,
+  AbstractControl,
+  Validators,
+  ValidatorFn
+} from "@angular/forms";
 
 @Component({
   selector: 'app-broad-lane',
@@ -7,7 +15,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BroadLaneComponent implements OnInit {
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit() {
   }
