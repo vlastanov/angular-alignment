4ea2e6b14479c8b3176fd4129f56235b810f0658
vlastanov <vlastanov@hotmail.com>
2020-05-18 14:11:23 +0300

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import {AppRoutingModule} from './app-routing.module';
 import { BroadLaneComponent } from './broad-lane/broad-lane.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule,AppRoutingModule ],
+  imports:      [ BrowserModule, FormsModule,AppRoutingModule, ReactiveFormsModule ],
   declarations: [ AppComponent, BroadLaneComponent,   ],
   bootstrap:    [ AppComponent ]
 })
