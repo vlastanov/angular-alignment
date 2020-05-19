d0bb2b823c89c5ad082cac006ba265871e48556e
vlastanov <vlastanov@hotmail.com>
2020-05-19 09:51:53 +0300

    1 file modified, 1 added

--- src/app/tablica/tablica.component.ts
+++ src/app/tablica/tablica.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { EChartOption } from 'echarts';
 
 @Component({
   selector: 'app-tablica',
@@ -6,6 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tablica.component.css']
 })
 export class TablicaComponent implements OnInit {
+
+  chartOption: EChartOption = {
+  xAxis: {
+    type: 'category',
+    data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+  },
+  yAxis: {
+    type: 'value',
+  },
+  series: [
+    {
+      data: [820, 932, 901, 934, 1290, 1330, 1320],
+      type: 'line',
+    },
+  ],
+};
 
   constructor() { }
 
