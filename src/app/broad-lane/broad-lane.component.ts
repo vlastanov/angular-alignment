48b6fa52d750c46286a2d7c23ab124bbc9f7b582
vlastanov <vlastanov@hotmail.com>
2020-05-20 10:47:43 +0300

    2 files modified

--- src/app/broad-lane/broad-lane.component.ts
+++ src/app/broad-lane/broad-lane.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from "@angular/core";
-import {  FormBuilder,  FormGroup,} from "@angular/forms";
+import { FormBuilder, FormGroup } from "@angular/forms";
 
 @Component({
   selector: "app-broad-lane",
@@ -8,14 +8,14 @@ import {  FormBuilder,  FormGroup,} from "@angular/forms";
 })
 export class BroadLaneComponent implements OnInit {
   form: FormGroup;
-  broadening=''
+  broadening = "";
   constructor(private fb: FormBuilder) {}
 
   ngOnInit() {
     this.form = this.fb.group({
       numberL: "2",
       width: "3.25",
-      radiusPr: "150"
+      radius: "150"
     });
   }
 
@@ -23,14 +23,14 @@ export class BroadLaneComponent implements OnInit {
     let input = {
       numberLanes: this.form.get("numberL").value,
       laneWidth: this.form.get("width").value,
-      radiusPr: this.form.get("radiusPr").value
+      radiusPr: this.form.get("radius").value
     };
     let calc = new Calculate(
       input.numberLanes,
       input.laneWidth,
       input.radiusPr
     );
-    this.broadening=calc.CalculateBroadening();
+    this.broadening = calc.CalculateBroadening();
   }
 }
 
@@ -43,8 +43,7 @@ class Calculate {
     private radiusPr: number
   ) {}
 
-  CalculateBroadening():string {
-
+  CalculateBroadening(): string {
     let widthAllLanes = this.numberLanes * this.laneWidth;
     if (widthAllLanes <= 6) {
       this.needBroadening = this.radiusPr >= 30 && this.radiusPr <= 400;
@@ -52,20 +51,19 @@ class Calculate {
       this.needBroadening = this.radiusPr >= 30 && this.radiusPr <= 200;
     }
 
-  let result=''
+    let result = "";
     if (this.needBroadening) {
       let maxE = (50 * this.numberLanes) / this.radiusPr;
 
-      if(maxE<=0.25){
-        return 'няма нужда от уширение, тъй като то е по-малко от 0.25 m'
+      if (maxE <= 0.25) {
+        return "няма нужда от уширение, тъй като то е по-малко от 0.25 m";
       }
       maxE = Math.round((maxE + Number.EPSILON) * 100) / 100;
-      result=`Уширението е: ${maxE} m`
+      result = `Уширението е: ${maxE} m`;
       console.log(maxE);
     } else {
-      result="няма нужда от уширение";
+      result = "няма нужда от уширение";
     }
-    return result
+    return result;
   }
 }
-
