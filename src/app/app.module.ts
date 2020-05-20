1e5d658f796e3ed8f0d40321f369e06900586f9a
vlastanov <vlastanov@hotmail.com>
2020-05-20 09:15:08 +0300

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
-import { AppComponent } from './app.component';
-import {AppRoutingModule} from './app-routing.module';
-import { BroadLaneComponent } from './broad-lane/broad-lane.component';
 import { ReactiveFormsModule } from '@angular/forms';
+
+import {AppRoutingModule} from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BroadLaneComponent } from './broad-lane/broad-lane.component';
 import { AuhilComponent } from './auhil/auhil.component';
 import { Test5Component } from './test5/test5.component';
 import { TablicaComponent } from './tablica/tablica.component';
+import { Test1Component } from './test1/test1.component';
+
 import * as echarts from 'echarts';
 import { NgxEchartsModule } from 'ngx-echarts';
-import { Test1Component } from './test1/test1.component';
 
 @NgModule({
   imports:      [ BrowserModule,   NgxEchartsModule.forRoot({ echarts }),   FormsModule,AppRoutingModule, ReactiveFormsModule ],
