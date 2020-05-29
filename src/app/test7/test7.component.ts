99845008f52ee4ab6c515b8d5f3f208ee86b7439
vlastanov <vlastanov@hotmail.com>
2020-05-29 11:51:01 +0300

    1 file modified

--- src/app/test7/test7.component.ts
+++ src/app/test7/test7.component.ts
@@ -36,11 +36,15 @@ export class Test7Component implements OnInit {
     this.thethaRad=this.Lp/(2*this.r);  
     this.thethGrad=(this.thethaRad*200)/Math.PI
 
-    let y=this.r - Math.sqrt(this.r*this.r -40*40)
+    this.getCircularPiketaj(40)
+
+
+
+  }
+
+  getCircularPiketaj(x){
+    let y=this.r - Math.sqrt(this.r*this.r -x*x)
     console.log(y)
-
-
-
   }
 
 }
