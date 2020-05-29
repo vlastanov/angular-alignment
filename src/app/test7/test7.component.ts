8dd81f5160b79f4788c909bfd86b0349f0e1a807
vlastanov <vlastanov@hotmail.com>
2020-05-29 11:01:35 +0300

    2 files modified

--- src/app/test7/test7.component.ts
+++ src/app/test7/test7.component.ts
@@ -6,8 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./test7.component.css']
 })
 export class Test7Component implements OnInit {
+
   r=50
   beta=50
+  delta
   tangenta
   bisektrisa
   D
@@ -20,12 +22,12 @@ export class Test7Component implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    let delta=(200-this.beta)
-    let deltaHalfRadians=(delta/2)*Math.PI/200
+    this.delta=(200-this.beta)
+    let deltaHalfRadians=(this.delta/2)*Math.PI/200
 
     this.tangenta=this.r*Math.tan(deltaHalfRadians)
     this.bisektrisa=this.r*(1/Math.cos(deltaHalfRadians)-1)
-    this.D=(Math.PI*this.r*delta)/200
+    this.D=(Math.PI*this.r*this.delta)/200
 
     this.A=this.r/3
     this.Lp=(this.A*this.A)/this.r
