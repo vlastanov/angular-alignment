0df72af2c1dd8a3090baac0949f5d1c0b697dd77
vlastanov <vlastanov@hotmail.com>
2020-05-29 15:01:47 +0300

    1 file modified

--- src/app/test7/test7.component.ts
+++ src/app/test7/test7.component.ts
@@ -1,32 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 import { FormBuilder, FormGroup } from "@angular/forms";
 
 @Component({
-  selector: 'app-test7',
-  templateUrl: './test7.component.html',
-  styleUrls: ['./test7.component.css']
+  selector: "app-test7",
+  templateUrl: "./test7.component.html",
+  styleUrls: ["./test7.component.css"]
 })
 export class Test7Component implements OnInit {
   form: FormGroup;
 
-  r=50
-  beta=50
-  delta
-  tangenta
-  bisektrisa
-  D
-  A
-  Lp
-  deltaR
-  thethaRad
-  thethGrad
+  r = 50;
+  beta = 50;
+  delta;
+  tangenta;
+  bisektrisa;
+  D;
+  A;
+  Lp;
+  deltaR;
+  thethaRad;
+  thethGrad;
 
-  xk
-  yk
+  xk;
+  yk;
 
   constructor(private fb: FormBuilder) {}
 
-    save() {
+  save() {
+    let input = {
+      r: this.form.get("r").value,
+      beta: this.form.get("beta").value,
+      A: this.form.get("A").value
+    };
   }
 
   ngOnInit() {
@@ -35,40 +40,41 @@ export class Test7Component implements OnInit {
       beta: 80,
       A: 40
     });
-    this.delta=(200-this.beta)
-    let deltaHalfRadians=(this.delta/2)*Math.PI/200
+    this.delta = 200 - this.beta;
+    let deltaHalfRadians = ((this.delta / 2) * Math.PI) / 200;
 
-    this.tangenta=this.r*Math.tan(deltaHalfRadians)
-    this.bisektrisa=this.r*(1/Math.cos(deltaHalfRadians)-1)
-    this.D=(Math.PI*this.r*this.delta)/200
+    this.tangenta = this.r * Math.tan(deltaHalfRadians);
+    this.bisektrisa = this.r * (1 / Math.cos(deltaHalfRadians) - 1);
+    this.D = (Math.PI * this.r * this.delta) / 200;
 
-    this.A=this.r/2
-    this.Lp=(this.A*this.A)/this.r
-    this.deltaR=Math.pow(this.Lp,2)/(24*this.r)
-    
-    this.thethaRad=this.Lp/(2*this.r);  
-    this.thethGrad=(this.thethaRad*200)/Math.PI
+    this.A = this.r / 2;
+    this.Lp = (this.A * this.A) / this.r;
+    this.deltaR = Math.pow(this.Lp, 2) / (24 * this.r);
+
+    this.thethaRad = this.Lp / (2 * this.r);
+    this.thethGrad = (this.thethaRad * 200) / Math.PI;
 
     // this.getCircularPiketaj(40)
-// console.log(Math.pow(3,3))
-    this.xk=this.Lp -Math.pow(this.Lp,5)/(40*Math.pow(this.Lp,4))
-    this.yk=Math.pow(this.Lp,3)/(6*Math.pow(this.Lp,2))- (Math.pow(this.Lp,7))/(336*Math.pow(this.Lp,6))
+    // console.log(Math.pow(3,3))
+    this.xk = this.Lp - Math.pow(this.Lp, 5) / (40 * Math.pow(this.Lp, 4));
+    this.yk =
+      Math.pow(this.Lp, 3) / (6 * Math.pow(this.Lp, 2)) -
+      Math.pow(this.Lp, 7) / (336 * Math.pow(this.Lp, 6));
 
-this.getSpiralPiketaj(45)
-
-
+    this.getSpiralPiketaj(45);
   }
 
-  getCircularPiketaj(x){
-    let y=this.r - Math.sqrt(this.r*this.r -x*x)
-    console.log(y)
+  getCircularPiketaj(x) {
+    let y = this.r - Math.sqrt(this.r * this.r - x * x);
+    console.log(y);
   }
 
-  getSpiralPiketaj(x){
-    let xk=x -Math.pow(x,5)/(40*Math.pow(x,4))
-    let yk=Math.pow(x,3)/(6*Math.pow(x,2))- (Math.pow(x,7))/(336*Math.pow(x,6))
-    console.log(xk)
-    console.log(yk)
+  getSpiralPiketaj(x) {
+    let xk = x - Math.pow(x, 5) / (40 * Math.pow(x, 4));
+    let yk =
+      Math.pow(x, 3) / (6 * Math.pow(x, 2)) -
+      Math.pow(x, 7) / (336 * Math.pow(x, 6));
+    console.log(xk);
+    console.log(yk);
   }
-
 }
