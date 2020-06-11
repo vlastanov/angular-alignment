6cbba205e54415ed938760fd900660d5abbe5035
vlastanov <vlastanov@hotmail.com>
2020-06-11 14:24:43 +0300

    1 file modified

--- src/app/test8/test8.component.ts
+++ src/app/test8/test8.component.ts
@@ -16,17 +16,11 @@ export class Test8Component implements OnInit {
     let start = new Point(+"8547251.3163", +"4554911.912");
     let end = new Point(+"8547258.1907", +"4554925.707");
 
+    let line=new Line(start, end)
+
     let triangle1 = new Triangle(start, end);
 
-    let rightAngleX = end.x - triangle1.PointRightAngle.x;
-    console.log(rightAngleX);
-    let rightAngleY = triangle1.PointRightAngle.y - start.y;
-    console.log(rightAngleY);
-    let cosAngle = rightAngleY / triangle1.DistanceHypotenuse;
-    console.log(cosAngle);
-
-    let angle = (Math.acos(cosAngle) * 200) / Math.PI;
-    console.log(angle);
+    console.log(triangle1.angleOne);
   }
 }
 
@@ -34,31 +28,55 @@ export class Point {
   constructor(public x: number, public y: number) {}
 }
 
+export class Line{
+  constructor(public start:Point, public end:Point){}  
+
+  private get DistanceX() {
+    return this.end.x - this.start.x;
+  }
+
+  private get DistanceY() {
+    return this.end.y - this.start.y;
+  }
+
+  get Distance(){    
+    return Math.sqrt(Math.pow(this.DistanceX, 2) + Math.pow(this.DistanceY, 2));
+  }
+}
+
 export class Triangle {
   constructor(private start: Point, private end: Point) {
-    console.log(this.DistanceX);
-    console.log(this.DistanceY);
-    console.log(this.DistanceHypotenuse);
+    // console.log(this.DistanceX);
+    // console.log(this.DistanceY);
+    // console.log(this.DistanceHypotenuse);`
   }
 
-  public get PointRightAngle(){
-
+  public get PointRightAngle() {
     let x = this.end.x;
     let y = this.start.y;
-    return new Point(x,y)
+    return new Point(x, y);
   }
 
-  public get DistanceX(){
-    return this.end.x - this.start.x
+  public get DistanceX() {
+    return this.end.x - this.start.x;
   }
 
-  public get DistanceY(){
-    return this.end.y - this.start.y
+  public get DistanceY() {
+    return this.end.y - this.start.y;
   }
 
-  public get DistanceHypotenuse(){
-    return  Math.sqrt(
-      Math.pow(this.DistanceX, 2) + Math.pow(this.DistanceY, 2)
-    )
+  public get DistanceHypotenuse() {
+    return Math.sqrt(Math.pow(this.DistanceX, 2) + Math.pow(this.DistanceY, 2));
+  }
+
+  public get angleOne() {
+    let rightAngleX = this.end.x - this.PointRightAngle.x;
+    let rightAngleY = this.PointRightAngle.y - this.start.y;
+    let cosAngle = rightAngleY / this.DistanceHypotenuse;
+    // console.log(rightAngleX);
+    // console.log(rightAngleY);
+    // console.log(cosAngle);
+
+    return (Math.acos(cosAngle) * 200) / Math.PI;
   }
 }
