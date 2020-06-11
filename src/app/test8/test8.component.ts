5d2a6df5baf2a2ae60db3c08f1e9e11d6b892770
vlastanov <vlastanov@hotmail.com>
2020-06-11 09:38:14 +0300

    1 file modified

--- src/app/test8/test8.component.ts
+++ src/app/test8/test8.component.ts
@@ -10,20 +10,35 @@ export class Test8Component implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    this.method()
   }
 
   method(){
     let startX='8547251.3163'
-    let startY='8547251.3163'
+    let startY='4554911.912'
     let endX='8547258.1907'
-    let endY='8547258.1907'
+    let endY='4554925.707'
+
+    let deltaX=+endX - +startX
+    console.log(deltaX)
+    let deltaY=+endY - +startY
+    console.log(deltaY)
+let distanceHypotenuse=Math.sqrt( Math.pow(deltaX,2)+Math.pow(deltaY,2))
 
 
-    let rightAngleX=+endX - +startX
-    let rightAngleY= +endY
+    let rightAngleX=+endX - +endX
+    let rightAngleY= +endY - +startY
+    let cosAngle=(rightAngleX/distanceHypotenuse)
 
-    console.log(rightAngleX)
-    console.log(rightAngleY)
+
+
+
+
+
+    
+
+    console.log(distanceHypotenuse)
+    console.log(cosAngle)
   }
 
 }
