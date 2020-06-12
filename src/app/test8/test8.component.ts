a83eb0c1f66bf561b62ee484ec6e6b6ce8c574ce
vlastanov <vlastanov@hotmail.com>
2020-06-12 14:30:59 +0300

    1 file modified

--- src/app/test8/test8.component.ts
+++ src/app/test8/test8.component.ts
@@ -13,13 +13,37 @@ export class Test8Component implements OnInit {
   }
 
   method() {
-    let one = new Point(+"10", +"2");
-    let two = new Point(+"5", +"5");
-    let three = new Point(+"-3", +"17");
+    let o1 = new Point(+"35.8559", +"35.7794");
+    let o2 = new Point(+"59.911", +"89.6766");
+    let o3 = new Point(+"88.7243", +"63.5206");
+    let o4 = new Point(+"111.9865", +"86.242");
+    let o5 = new Point(+"127.3183", +"73.8245");
+    let o6 = new Point(+"110.9291", +"54.2736");
+    let o7 = new Point(+"99.8267", +"59.0292");
+    let o8 = new Point(+"88.9887", +"43.4413");
+    let o9 = new Point(+"62.8188", +"50.0463");
+
     let four = new Point(+"8547277.1857", +"4554902.6646");
-    let tr1 = new RightTriangle(one, two);
-    let tr2 = new RightTriangle(two, three);
-    let polVrah = new PoligonVrah(tr1, tr2);
+    let tr1 = new RightTriangle(o1, o2);
+    let tr2 = new RightTriangle(o2, o3);
+    let tr3 = new RightTriangle(o3, o4);
+    let tr4 = new RightTriangle(o4, o5);
+    let tr5 = new RightTriangle(o5, o6);
+    let tr6 = new RightTriangle(o6, o7);
+    let tr7 = new RightTriangle(o7, o8);
+    let tr8 = new RightTriangle(o8, o9);
+    let tr9 = new RightTriangle(o9, o1);
+
+    let po1 = new PoligonVrah(tr1, tr2);
+    let po2 = new PoligonVrah(tr2, tr3);
+    let po3 = new PoligonVrah(tr3, tr3);
+    let po4 = new PoligonVrah(tr3, tr4);
+    let po5 = new PoligonVrah(tr4, tr5);
+    let po6 = new PoligonVrah(tr5, tr6);
+    let po7 = new PoligonVrah(tr6, tr7);
+    let po8 = new PoligonVrah(tr7, tr8);
+    let po9 = new PoligonVrah(tr8, tr9);
+    let po10 = new PoligonVrah(tr9, tr1);
   }
 }
 
